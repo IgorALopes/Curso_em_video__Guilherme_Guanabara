bc83ad18e0d56f4e1986dd5a2c81ef08bda342ea
Igor de Andrade Lopes <96532725+IgorALopes@users.noreply.github.com>
2023-06-24 23:05:49 -0300

    test change

--- CursoPython/PythonTeste/teste01.py
+++ CursoPython/PythonTeste/teste01.py
@@ -2,3 +2,7 @@ nome = input('Qual é o seu nome? ')
 idade = input('Qual é a sua idade? ')
 peso = input('Qual é o seu peso? ')
 print(nome, idade, peso)
+
+for x in range(10):
+    for y in range(x):
+        print(y)
